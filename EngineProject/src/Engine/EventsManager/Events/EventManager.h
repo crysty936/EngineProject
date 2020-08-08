79b2037673cc1bdf37d884e2f41ef1275c78cc1a
Bobescu Cristian <crysty936@gmail.com>
2020-08-08 19:50:26 +0300

    *replaced std::bind with lambda

--- EngineProject/src/Engine/EventsManager/Events/EventManager.h
+++ EngineProject/src/Engine/EventsManager/Events/EventManager.h
@@ -7,7 +7,7 @@
 #include "Engine/EventsManager/Events/KeyEvents.h"
 #include "Engine/EventsManager/EventDispatcher.h"
 
-#define BIND_FUNC_EVT(fn) std::bind(&fn,this,std::placeholders::_1),this
+#define BIND_FUNC_EVT(fn) [this](auto&&... args) -> decltype(auto){ return this->fn(std::forward<decltype(args)>(args)...); }, this
 #define BIND_FUNC_SIMPLE(fn) std::bind(&fn,this,std::placeholders::_1)
 
 
